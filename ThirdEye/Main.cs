2701156b191f1019275cb85b1e3e7b494377e713
Niko Fox <niko@fauxnik.me>
2023-08-12 17:11:13 -0700

    Convert to AmalgamCamera
    AmalgamCamera abstracts the fact that multiple cameras are needed to properly render the scene.
    Using it makes managing the cameras simpler as it reduces the number of objects we must store and interact with from three down to one.

--- ThirdEye/Main.cs
+++ ThirdEye/Main.cs
@@ -1,6 +1,5 @@
 ﻿using CameraManager;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.Object;
 using UnityModManagerNet;
@@ -12,13 +11,7 @@ namespace ThirdEye;
 public static class Main
 {
 	public static Settings settings = new Settings();
-	public static readonly string CAMERA1_NAME = "Camera (third eye)";
-	public static readonly string CAMERA2_NAME = "SecondCamera (third eye)";
-	public static readonly string CAMERA3_NAME = "ThirdCamera (third eye)";
-	private static readonly int DEPTH_OFFSET = 10;
-	private static Camera? camera1;
-	private static Camera? camera2;
-	private static Camera? camera3;
+	private static AmalgamCamera? thirdEye;
 
 	// Unity Mod Manage Wiki: https://wiki.nexusmods.com/index.php/Category:Unity_Mod_Manager
 	private static bool Load(ModEntry modEntry)
@@ -56,13 +49,9 @@ public static class Main
 
 	private static void OnSettingsChanged()
 	{
-		Camera?[] cameras = { camera1, camera2, camera3 };
-		foreach (Camera? camera in cameras)
-		{
-			if (camera == null) { continue; }
-			camera.fieldOfView = settings.fieldOfView;
-			camera.nearClipPlane = settings.nearClipPlane;
-		}
+		if (thirdEye == null) { return; }
+		thirdEye.fieldOfView = settings.fieldOfView;
+		thirdEye.nearClipPlane = settings.nearClipPlane;
 	}
 
 	private static void OnPlayerCameraChanged()
@@ -75,52 +64,17 @@ public static class Main
 	{
 		if (on)
 		{
-			// Derail Valley uses 3 cameras
-			//   1. Camera (eye) - renders most things, depth 0 (Layers 267386643)
-			//   2. SecondCamera - renders ???, depth 1 (Layer 32)
-			//   3. ThridCamera  - renders UI, depth 2 (Layer 4)
-			Camera playerCamera = PlayerManager.PlayerCamera;
-			Camera secondCamera = new List<Camera>(Camera.allCameras).Find(cam => cam.name == "SecondCamera");
-			Camera thirdCamera = new List<Camera>(Camera.allCameras).Find(cam => cam.name == "ThirdCamera");
-
-			if (playerCamera == null || secondCamera == null || thirdCamera == null)
-			{
-				// TODO: log error
-				return;
-			}
-
-			camera1 = CloneCamera(playerCamera, CAMERA1_NAME);
-			camera2 = CloneCamera(secondCamera, CAMERA2_NAME);
-			camera3 = CloneCamera(thirdCamera, CAMERA3_NAME);
+			thirdEye = new GameObject() { name = "ThirdEye" }.AddComponent<AmalgamCamera>();
+			thirdEye.Init(CameraManager.CameraType.All);
+			thirdEye.gameObject.transform.SetParent(PlayerManager.PlayerCamera.gameObject.transform, false);
+			thirdEye.stereoTargetEye = StereoTargetEyeMask.None;
+			thirdEye.fieldOfView = settings.fieldOfView;
+			thirdEye.nearClipPlane = settings.nearClipPlane;
 			return;
 		}
 
-		if (camera1 != null)
-		{
-			Destroy(camera1.gameObject);
-			camera1 = null;
-		}
-		if (camera2 != null)
-		{
-			Destroy(camera2.gameObject);
-			camera2 = null;
-		}
-		if (camera3 != null)
-		{
-			Destroy(camera3.gameObject);
-			camera3 = null;
-		}
-	}
-
-	private static Camera CloneCamera(Camera source, string name)
-	{
-		Camera camera = CameraAPI.CloneCamera(source, false);
-		camera.gameObject.transform.SetParent(source.gameObject.transform, false);
-		camera.gameObject.name = name;
-		camera.stereoTargetEye = StereoTargetEyeMask.None;
-		camera.depth += DEPTH_OFFSET;
-		camera.fieldOfView = settings.fieldOfView;
-		camera.nearClipPlane = settings.nearClipPlane;
-		return camera;
+		if (thirdEye == null) { return; }
+		Destroy(thirdEye);
+		thirdEye = null;
 	}
 }
